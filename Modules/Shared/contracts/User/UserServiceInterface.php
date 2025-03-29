59e20d3556e0fe55eae70030acda7c4ecb1626b3
alimoharrami <alimoharrmai700@gmail.com>
2025-03-29 18:09:16 +0330

    [Add]: Shared, Add user service interface details.

--- Modules/Shared/contracts/User/UserServiceInterface.php
+++ Modules/Shared/contracts/User/UserServiceInterface.php
@@ -2,7 +2,24 @@
 
 namespace App\Shared\Contracts\User;
 
+use Modules\Shared\DTOs\User\UserDTO;
+
+
 interface UserServiceInterface
 {
-    //todo
+    /**
+     * get user dto with user id
+     *
+     * @param int $userId
+     * @return UserDTO|null
+     */
+    public function getUserDTO(int $userId): ?UserDTO;
+
+    /**
+     * get many users DTOs
+     *
+     * @param array $userIds
+     * @return array
+     */
+    public function getUserDTOs(array $userIds): array;
 }
