f00d2bc19f341739786f663524f1d74f72d52e58
alimoharrami <alimoharrmai700@gmail.com>
2025-03-29 18:36:38 +0330

    [Chore]: Product, Add product, product category controller structure.

--- Modules/Product/app/Http/Controllers/ProductController.php
+++ Modules/Product/app/Http/Controllers/ProductController.php
@@ -12,45 +12,14 @@ class ProductController extends Controller
      */
     public function index()
     {
-        return view('product::index');
+        //todo return all products
     }
-
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        return view('product::create');
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request) {}
 
     /**
      * Show the specified resource.
      */
     public function show($id)
     {
-        return view('product::show');
+        //todo return product
     }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit($id)
-    {
-        return view('product::edit');
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, $id) {}
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy($id) {}
 }
