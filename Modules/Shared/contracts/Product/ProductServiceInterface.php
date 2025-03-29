39fb9e21e802cbf2d3709209cb1e20662d65d6e7
alimoharrami <alimoharrmai700@gmail.com>
2025-03-29 18:41:33 +0330

    [Chore]: Shared, Add product service interface.

--- Modules/Shared/contracts/Product/ProductServiceInterface.php
+++ Modules/Shared/contracts/Product/ProductServiceInterface.php
@@ -2,7 +2,23 @@
 
 namespace App\Shared\Contracts\Product;
 
+use Modules\Shared\DTOs\Product\ProductDTO;
+
 interface ProductServiceInterface
 {
-    //todo
+    /**
+     * get product dto with product id
+     *
+     * @param int $productId
+     * @return ProductDTO|null
+     */
+    public function getProductDTO(int $productId): ?productDTO;
+
+    /**
+     * get many product DTOs
+     *
+     * @param array $productIds
+     * @return array
+     */
+    public function getProductDTOs(array $productIds): array;
 }
