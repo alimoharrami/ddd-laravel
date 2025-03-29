1ec8d66e4cc34dc685f8a35a26068eec53fc5e3d
alimoharrami <alimoharrmai700@gmail.com>
2025-03-29 18:33:17 +0330

    [Add]: Product, Add product, product category models.

--- Modules/Product/models/Product.php
+++ Modules/Product/models/Product.php
@@ -3,8 +3,16 @@
 namespace App\Modules\Product\Domain\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Product extends Model
 {
-    //todo
+    protected $fillable = [
+        'name', 'slug', 'description', 'price', 'stock', 'category_id', 'status'
+    ];
+
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(ProductCategory::class);
+    }
 }
--- Modules/Product/models/ProductCategory.php
+++ Modules/Product/models/ProductCategory.php
@@ -3,8 +3,14 @@
 namespace App\Modules\Product\Domain\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class ProductCategory extends Model
 {
-    //todo
+    protected $fillable = ['name', 'slug', 'description'];
+
+    public function products(): HasMany
+    {
+        return $this->hasMany(Product::class);
+    }
 }
