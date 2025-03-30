907a34a11c637e883304381b9a3ee599428028dd
alimoharrami <alimoharrmai700@gmail.com>
2025-03-30 19:59:32 +0330

    [Add]: Cart, Add cart and cart item models.

--- Modules/Cart/models/Cart.php
+++ Modules/Cart/models/Cart.php
@@ -3,8 +3,19 @@
 namespace App\Modules\Product\Domain\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Cart extends Model
 {
-    //todo
+    protected $fillable = ['user_id'];
+
+    //todo cart status
+
+    /**
+     * Get the cart items for the cart.
+     */
+    public function items(): HasMany
+    {
+        return $this->hasMany(CartItem::class);
+    }
 }
