dd9e0cb7cb384e5b365a32bcdd0901626f4a3a3c
alimoharrami <alimoharrmai700@gmail.com>
2025-03-31 14:52:30 +0330

    [Chore]: Order, add controller structures.

--- Modules/Order/app/Http/Controllers/OrderController.php
+++ Modules/Order/app/Http/Controllers/OrderController.php
@@ -10,21 +10,8 @@ class OrderController extends Controller
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function getOrderList()
     {
         //todo return orders
-    }
-
-    /**
-     * Show the specified resource.
-     */
-    public function disableOrder($id)
-    {
-        //todo make order disable
-    }
-
-    public function update($id)
-    {
-        //todo update order amounts
     }
 }
