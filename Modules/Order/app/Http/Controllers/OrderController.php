9cf6a68937ef99415e9bed0442d74af03f227af1
alimoharrami <alimoharrmai700@gmail.com>
2025-03-31 16:07:32 +0330

    [Add]: Order, add order controller.

--- Modules/Order/app/Http/Controllers/OrderController.php
+++ Modules/Order/app/Http/Controllers/OrderController.php
@@ -3,15 +3,20 @@
 namespace Modules\Order\Http\Controllers;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use App\Modules\Product\Domain\Models\Order;
+use Illuminate\Http\JsonResponse;
 
 class OrderController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function getOrderList()
+    public function index(): JsonResponse
     {
-        //todo return orders
+        $orders = Order::all();
+
+        return response()->json([
+            'orders' => $orders
+        ]);
     }
 }
--- Modules/Order/routes/api.php
+++ Modules/Order/routes/api.php
@@ -4,5 +4,5 @@ use Illuminate\Support\Facades\Route;
 use Modules\Order\Http\Controllers\OrderController;
 
 Route::middleware(['auth:sanctum'])->prefix('v1')->group(function () {
-    Route::apiResource('order', OrderController::class)->names('order');
+    Route::get('/orders', [OrderController::class, 'index']);
 });
